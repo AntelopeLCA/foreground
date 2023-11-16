94b1afe1fed8b532b5939a7a3a1ccdd3a18a5047
Brandon Kuczenski <brandon.kuczenski@301south.net>
2023-11-15 17:32:29 -0800

    Convert FragmentFlowModels to operable FragmentFlows

--- antelope_foreground/foreground_query.py
+++ antelope_foreground/foreground_query.py
@@ -3,6 +3,10 @@ from antelope_core.catalog_query import CatalogQuery
 from antelope_core.contexts import NullContext
 
 from .interfaces.iforeground import AntelopeForegroundInterface
+from .models import FragmentFlow as FragmentFlowModel
+from .fragment_flows import FragmentFlow
+from .terminations import FlowTermination
+
 
 
 class ForegroundNotSafe(Exception):
@@ -14,8 +18,47 @@ class ForegroundNotSafe(Exception):
 
 class ForegroundQuery(CatalogQuery, AntelopeForegroundInterface):
     """
-    Add foreground interface to query object
+    Add foreground interface to query object.
+    We also need to add lubricating code to translate between pydantic models and operable objects
     """
+    def _make_fragment_flow(self, ff_model):
+        if isinstance(ff_model, FragmentFlowModel):
+            frag = self.make_ref(self.get(ff_model.fragment.entity_id, origin=ff_model.fragment.origin))
+
+            # we have to do this manually because legacy code is terrible
+            if ff_model.term.is_null:
+                term = FlowTermination.null(frag)
+            else:
+                if ff_model.anchor.anchor_flow:
+                    term_flow = self.make_ref(self.get(ff_model.anchor.anchor_flow.entity_id,
+                                                       origin=ff_model.anchor.anchor_flow.origin))
+                else:
+                    term_flow = None
+                if ff_model.anchor.context:
+                    term = FlowTermination(frag, self.get_context(ff_model.anchor.context), term_flow=term_flow,
+                                           descend=ff_model.anchor.descend)
+                elif ff_model.anchor.node:
+                    term_node = self.make_ref(self.get(ff_model.anchor.node.entity_id,
+                                                       origin=ff_model.anchor.node.origin))
+                    term = FlowTermination(frag, term_node, term_flow=term_flow,
+                                           descend=ff_model.anchor.descend)
+                else:
+                    term = FlowTermination.null(frag)
+            if ff_model.term.score_cache:
+                ar = self._catalog.get_archive(self.origin)
+                term._deserialize_score_cache(ar, ff_model.anchor.score_cache, ff_model.anchor_scenario)
+
+            return FragmentFlow(frag, ff_model.magnitude, ff_model.node_weight, term,
+                                ff_model.is_conserved, match_ev=ff_model.scenario, match_term=ff_model.anchor_scenario)
+        return ff_model
+
+    def traverse(self, fragment, scenario=None, **kwargs):
+        ffs = super(ForegroundQuery, self).traverse(fragment, scenario=scenario, **kwargs)
+        return [self._make_fragment_flow(ff) for ff in ffs]
+
+    def activity(self, fragment, scenario=None, **kwargs):
+        ffs = super(ForegroundQuery, self).activity(fragment, scenario=scenario, **kwargs)
+        return [self._make_fragment_flow(ff) for ff in ffs]
 
     def fragment_lcia(self, fragment, quantity_ref, scenario=None, **kwargs):
         ress = super(ForegroundQuery, self).fragment_lcia(fragment, quantity_ref, scenario=scenario, **kwargs)
@@ -29,7 +72,7 @@ class ForegroundQuery(CatalogQuery, AntelopeForegroundInterface):
             return self.get(item)
         except EntityNotFound:
             return None  # I know, it's so bad. Plan is to break this and use downstream errors to expunge the practice
-        
+
 
 class QueryIsDelayed(InvalidQuery):
     """
